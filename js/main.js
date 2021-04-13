8588e2c90f71238d03b51f27c47aa8a41095507f
Flavio Monteiro <flaviohnm@gmail.com>
2021-04-13 10:06:19 -0300

    Projeto Finalizado

--- js/main.js
+++ js/main.js
@@ -12,8 +12,8 @@ let direction = "right";
 
 let food = {
 
-    x = Math.floor(Math.random() * 15 + 1) * box,
-    y = Math.floor(Math.random() * 15 + 1) * box,
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
 
 }
 
@@ -38,7 +38,7 @@ function drawFood() {
 document.addEventListener('keydown', update);
 
 function update(event) {
-
+    
     if (event.keyCode == 37 && direction != "right") direction = "left";
     if (event.keyCode == 38 && direction != "down") direction = "up";
     if (event.keyCode == 39 && direction != "left") direction = "right";
@@ -53,6 +53,13 @@ function iniciarJogo() {
     if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
+    for (i = 1; i < snake.length; i++) {
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+    }
+
     criarBG();
     criarCobrinha();
     drawFood();
@@ -65,15 +72,11 @@ function iniciarJogo() {
     if (direction == "up") snakeY -= box;
     if (direction == "down") snakeY += box;
 
-    if(snakeX != food.x || snakeY != food.y){
-
+    if (snakeX != food.x || snakeY != food.y) {
         snake.pop();
-
-    }else{
-
+    } else {
         food.x = Math.floor(Math.random() * 15 + 1) * box;
         food.y = Math.floor(Math.random() * 15 + 1) * box;
-
     }
 
     let newHead = {
