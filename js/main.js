509dcf913d9abb7abe16f37ad5f8106aa8f90e35
Flavio Monteiro <flaviohnm@gmail.com>
2021-06-04 18:42:58 -0300

    building do projeto

--- js/main.js
+++ js/main.js
@@ -11,10 +11,8 @@ snake[0] = {
 let direction = "right";
 
 let food = {
-
     x: Math.floor(Math.random() * 15 + 1) * box,
     y: Math.floor(Math.random() * 15 + 1) * box
-
 }
 
 function criarBG() {
@@ -30,7 +28,6 @@ function criarCobrinha() {
 }
 
 function drawFood() {
-
     context.fillStyle = "red";
     context.fillRect(food.x, food.y, box, box);
 }
@@ -38,16 +35,14 @@ function drawFood() {
 document.addEventListener('keydown', update);
 
 function update(event) {
-    
     if (event.keyCode == 37 && direction != "right") direction = "left";
     if (event.keyCode == 38 && direction != "down") direction = "up";
     if (event.keyCode == 39 && direction != "left") direction = "right";
     if (event.keyCode == 40 && direction != "up") direction = "down";
-
 }
 
 function iniciarJogo() {
-
+    
     if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
